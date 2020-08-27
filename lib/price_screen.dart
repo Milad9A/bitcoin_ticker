59c387f3c55fa8a059cf5f3c0725603045dd6c38
Milad Awad <millad.99.awad@gmail.com>
2020-08-27 16:41:39 +0300

    Added Dropdown for Android and Picker for IOS

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -48,14 +48,6 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  //TODO: Create a method here called getData() to get the coin data from coin_data.dart
-
-  @override
-  void initState() {
-    super.initState();
-    //TODO: Call getData() when the screen loads up.
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,7 +69,6 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  //TODO: Update the Text Widget with the live bitcoin data here.
                   '1 BTC = ? USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
